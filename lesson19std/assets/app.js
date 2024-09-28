c28c0bd0739bc1bd9d4d367a4653da8a6192eed7
Ibrahim Quliyev <153994775+Quliyev02@users.noreply.github.com>
2024-09-29 03:12:22 +0400

    Add files via upload

---
+++ lesson19std/assets/app.js
@@ -0,0 +1,30 @@
+function Student(name, subject, grade = 0) {
+    this.name = name;
+    this.subject = subject;
+    this.grade = grade;
+
+    this.study = function(hours) {
+        this.grade += hours * 5;
+    };
+
+    this.getGrade = function() {
+        return this.grade;
+    };
+
+    this.changeSubject = function(newSubject) {
+        this.subject = newSubject;
+    };
+}
+
+// creating student with new keyword
+let student1 = new Student('John Doe', 'Calculus');
+
+// uptading grade 
+student1.study(3);
+
+// changing subject
+student1.changeSubject('Operating Systems');
+
+// console log for display results
+console.log(student1.getGrade()); // op 15
+console.log(student1.subject); // op operating systems
